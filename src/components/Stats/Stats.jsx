4115231a6253d0174240f8fbdc981953e31892af
Cyril Chan <kwunming0911@gmail.com>
2025-06-20 20:28:44 +0100

    conditional rendering

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,6 +1,7 @@
 import styles from './Stats.module.css'
 
 export default function Stats({items}) {
+    if (!items.length) return <p className={styles.stats}><em>Start Adding some items to your packing list</em></p>
     const total = items?.length ?? 0
     const packed = items?.reduce((acc, cur) => cur.packed ? acc + 1 : acc, 0) ?? 0
     const packedPercentage = Math.round((packed / total) * 100)
